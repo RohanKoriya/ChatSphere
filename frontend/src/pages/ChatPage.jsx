77dedfa16e14a89125dd8b1341ea824e6a6752ed
RohanKoriya <koriyarohan123@gmail.com>
2026-01-16 19:25:28 +0530

    login & logout implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatPage() {
-  return <div>ChatPage</div>;
+  const { logout } = useAuthStore();
+  return (
+    <div className="z-10">
+      <button
+        className="px-4 py-2 text-white bg-red-500 rounded-md"
+        onClick={logout}
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default ChatPage;
